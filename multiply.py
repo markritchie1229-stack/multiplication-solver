917f707676ca79380cbb28553b9ab335c2960313
markritchie1229-stack <markritchie1229@gmail.com>
2025-10-08 01:22:37 -0400

    Create multiply.py

---
+++ multiply.py
@@ -0,0 +1,8 @@
+def multiply(a, b):
+    return a * b
+
+# Example use:
+x = float(input("Enter the first number: "))
+y = float(input("Enter the second number: "))
+
+print(f"The product of {x} and {y} is {multiply(x, y)}")
